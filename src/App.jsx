83dc05bb5454a895a3ea2c78b777db9c7b7f21d0
sreeram051 <sreeram8122@gmail.com>
2024-05-01 13:08:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import Player from './components/Player';
+
+const  App = () => {
+  return (
+    <>
+  <div className='App'>
+    <Player/>
+  </div>
+</>
+  )
+}
+
+export default App;
